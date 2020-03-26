08bf147bd436979a551c191ded4459ceb8bbfec9
Luís Campos <luismmc1997@hotmail.com>
2020-03-26 05:07:23 +0000

    Configuração do Firebase

--- src/main.js
+++ src/main.js
@@ -1,7 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
+
+// App's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyAFdaXfk4neMblum6xMq9sL3o33XN4MDho",
+  authDomain: "peebu-b1d74.firebaseapp.com",
+  databaseURL: "https://peebu-b1d74.firebaseio.com",
+  projectId: "peebu-b1d74",
+  storageBucket: "peebu-b1d74.appspot.com",
+  messagingSenderId: "293436232690",
+  appId: "1:293436232690:web:85a93cdaa662141280d16b",
+  measurementId: "G-9MDGS3X3JX"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 new Vue({
   render: h => h(App),
