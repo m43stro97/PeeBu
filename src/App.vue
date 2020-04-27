05d56b4975ff0fd416b08b8e34fcc10ad9d60b5d
daniel rodrigues <m43stro97>
2020-04-27 18:43:18 +0100

    Navigation bar created

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,86 @@
 <template>
-  <v-app>
-    <div>
-      <v-toolbar
-        dark
+  <div id="app" class="container-fluid">
+    <v-app id="inspire">
+      <v-navigation-drawer v-model="drawer" :src="bg" app>
+        <v-list dense nav class="py-0">
+          <v-list-item two-line :class="miniVariant && 'px-0'">
+            <v-list-item-avatar> </v-list-item-avatar>
+
+            <v-list-item-content>
+              <v-list-item-title class="white--text">PeeBu</v-list-item-title>
+              <v-list-item-subtitle class="white--text"
+                >Project of Quality Software</v-list-item-subtitle
+              >
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-divider></v-divider>
+
+          <v-list-item v-for="item in items" :key="item.title">
+            <v-list-item-icon color="white">
+              <v-icon color="white">{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+              <v-list-item-title class="white--text">{{
+                item.title
+              }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+      <v-app-bar
+        app
+        fixed
+        dense
+        src="https://cdn.vuetifyjs.com/images/backgrounds/bg-2.jpg"
         prominent
-        src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
+        extended
       >
-        <v-app-bar-nav-icon></v-app-bar-nav-icon>
-
-        <v-toolbar-title>PeeBu</v-toolbar-title>
-
+        <v-app-bar-nav-icon
+          class="white--text"
+          @click.stop="drawer = !drawer"
+        ></v-app-bar-nav-icon>
+        <v-toolbar-title class="white--text">
+          PeeBu
+        </v-toolbar-title>
         <v-spacer></v-spacer>
-      </v-toolbar>
-      <LandingPage/>
-    </div>
-  </v-app>
+      </v-app-bar>
+
+      <v-content fill-height fluid>
+        <LandingPage/>
+      </v-content>
+    </v-app>
+  </div>
 </template>
 
 <script>
-import LandingPage from './components/LandingPage';
+import LandingPage from "./components/LandingPage";
 
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
     LandingPage,
   },
 
   data: () => ({
-    //
+    drawer: false,
+    items: [
+      { title: "Home", icon: "mdi-home" },
+      { title: "Statistics", icon: "mdi-chart-line" },
+      { title: "About", icon: "mdi-help-box" },
+    ],
+    miniVariant: false,
+    expandOnHover: false,
+    background: true,
   }),
+  computed: {
+    bg() {
+      return this.background
+        ? "https://cdn.vuetifyjs.com/images/backgrounds/bg-2.jpg"
+        : undefined;
+    },
+  },
 };
 </script>
