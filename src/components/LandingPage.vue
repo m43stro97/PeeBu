6d6bead11479faa77f35477afc114149a9e64ee4
daniel rodrigues <m43stro97>
2020-05-04 18:35:29 +0100

    Add actual balance label

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,7 +1,12 @@
 <template>
-  <v-app>
-    <h1>Home</h1>
-  </v-app>
+  <h2 class="balance">Balance:
+    <span class="a" v-if="balance > 0">
+      <label class="up">{{balance}}€</label>
+    </span>
+    <span class="a" v-if="balance < 0">
+      <label class="low">{{balance}}€</label>
+    </span>
+  </h2>
 </template>
 <script>
 export default {
@@ -10,7 +15,24 @@ export default {
  
 
   data: () => ({
-    //
+    balance: '1000'
   }),
 };
 </script>
+
+<style scoped>
+
+  
+  .balance{
+    text-align: end;
+  }
+
+  .up{
+    color: green;
+  }
+
+  .low{
+    color: red;
+  } 
+
+</style>
